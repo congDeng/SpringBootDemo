9f0902f9217fd0d5ace8c7b4dd9dea858bf99164
congDeng <congcong.deng@thoughtworks.com>
2024-08-15 11:11:24 +0800

    IOC and DI

---
+++ src/main/java/com/example/app/SpringBootDemoApplication.java
@@ -0,0 +1,21 @@
+package com.example.app;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootDemoApplication {
+	public static void main(String[] args) {
+		ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringBootDemoApplication.class, args);
+		Alien alien = applicationContext.getBean(Alien.class);
+		alien.complie();
+	}
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+}
